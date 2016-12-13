33b414dda4dd89980c3e6d291e2fb7041c6d3168
Nico <nicociani09@gmail.com>
2016-12-13 16:52:10 -0500

    fixed columns and nav

--- prototypes/ocs.js
+++ prototypes/ocs.js
@@ -1,22 +1,10 @@
-// selected elements
-var navTrigger = document.getElementById('nav-trigger');
-var nav = document.getElementById('nav');
-var labels = document.getElementsByClassName('nav-label');
+$(document).ready(function() {
+  $('body').addClass('js');
+  var $menu = $('#menu'),
+    $menulink = $('.menu-link');
 
-// Event Listening
-navTrigger.addEventListener('click', navToggle);
-window.addEventListener('resize', resize);
-
-
-function navToggle(e) {
-  var closed = (navTrigger.className.indexOf('close') > 0);
-  if(closed) {
-    navTrigger.className = 'nav-trigger open';
-    nav.className = 'out';
-  } else {
-    navTrigger.className = 'nav-trigger close';
-    nav.className = 'in';
-  }
-}
-
-window.onload = resize;
+$menulink.click(function() {
+  $menulink.toggleClass('active');
+  $menu.toggleClass('active');
+  return false;
+});});
